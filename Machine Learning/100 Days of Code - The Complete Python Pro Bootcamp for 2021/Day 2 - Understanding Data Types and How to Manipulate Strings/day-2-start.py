b3653c0c8ef90a3b60d51da31d8a9fa12362431a
fransHalason <franshalasonn@gmail.com>
2021-09-06 00:24:01 +0700

    day 2 - understanding data types and how to manipulate strings

---
+++ Machine Learning/100 Days of Code - The Complete Python Pro Bootcamp for 2021/Day 2 - Understanding Data Types and How to Manipulate Strings/day-2-start.py
@@ -0,0 +1,23 @@
+# Data Types
+
+# String
+
+print("Hello"[4])
+print("hello " + "world")
+
+# Integer
+
+print(123 + 456)
+print(123_456_789)
+
+# Float
+
+3.14159
+
+# Boolean
+
+True
+False
+
+street_name = "Abbey Road"
+print(street_name[4] + street_name[7])
