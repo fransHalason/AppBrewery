41d7752a231a78e2af7f7ef198cdd9c8851d2dcb
fransHalason <franshalasonn@gmail.com>
2021-09-08 23:26:42 +0700

    randomisation and python lists

---
+++ Machine Learning/100 Days of Code - The Complete Python Pro Bootcamp for 2021/Day 4 - Randomisation and Python Lists/day-4-2-exercise.py
@@ -0,0 +1,15 @@
+import random
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+#Get the total number of items in list.
+num_items = len(names)
+#Generate random numbers between 0 and the last index.
+random_choice = random.randint(0, num_items - 1)
+person_who_will_pay = names[random_choice]
+
+# person_who_will_pay = random.choice(names)
+print(person_who_will_pay + " is going to buy the meal today!")
---
+++ Machine Learning/100 Days of Code - The Complete Python Pro Bootcamp for 2021/Day 4 - Randomisation and Python Lists/day-4-list-pratice.py
@@ -0,0 +1,45 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine",
+                     "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+print(states_of_america)
+
+
+dirty_dozen0 = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples",
+                "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen0[1] = "Mangoes"
+
+dirty_dozen0.extend(["Oranges", "Durian"])
+
+print(dirty_dozen0)
+
+# print(dirty_dozen0[:-3])
+
+dirty_dozen2 = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples",
+                "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
+fruits = ["Strawberries", "Nectarines", "Apples",
+          "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", " Celery", "Potatoes"]
+
+dirty_dozen1 = [fruits, vegetables]
+
+print(dirty_dozen1)
+
+fruits = ["Strawberries", "Nectarines", "Apples",
+          "Grapes", "Peaches", "Cherries", "Pears"]
+fruits[-1] = "Melons"
+fruits.append("Lemons")
+print(fruits)
+
+fruits = ["Strawberries", "Nectarines", "Apples",
+          "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen)
+print(dirty_dozen[0])
+print(dirty_dozen[1])
+print(dirty_dozen[1][2])
+print(dirty_dozen[1][3])
