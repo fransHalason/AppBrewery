57d714c71ea92331222ab9eeebaa3f0f5cfa5430
fransHalason <franshalasonn@gmail.com>
2021-09-13 00:42:11 +0700

    day 9 - dictionaries, nesting, and the secret auction

---
+++ Machine Learning/100 Days of Code - The Complete Python Pro Bootcamp for 2021/Day 9 - Dictionaries, Nesting and the Secret Auction/day-9-start.py
@@ -0,0 +1,22 @@
+programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.",
+                          "Function": "A piece of code that you can easily call over and over again."}
+
+# Retrieving items from dictionary
+programming_dictionary["Bug"]
+
+# Adding new items to dictionary
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+
+# Create an empty dictionary
+empty_dictionary = {}
+
+# Wipe an existing dictionary
+# programming_dictionary = {}
+
+# Edit an item in a dictionary
+programming_dictionary["Bug"] = "A moth in your computer."
+
+# Loop through a dictionary
+for key in programming_dictionary:
+  print(key)
+  print(programming_dictionary[key])
