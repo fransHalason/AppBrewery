06b50b3fb1e9681a552d71d3c04dfdc0be36b496
fransHalason <franshalasonn@gmail.com>
2021-09-24 12:46:21 +0700

    day 19 - instances, state and higher order functions

---
+++ Machine Learning/100 Days of Code - The Complete Python Pro Bootcamp for 2021/Day 19 - Instances, State and Higher Order Functions/Etch a Sketch - Start/main.py
@@ -0,0 +1,38 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+
+def move_backwards():
+    tim.backward(10)
+
+
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
+screen.listen()
+screen.onkey(move_forwards, "w")
+screen.onkey(move_forwards, "s")
+screen.onkey(move_forwards, "a")
+screen.onkey(move_forwards, "d")
+screen.onkey(clear, "c")
+screen.exitonclick()
