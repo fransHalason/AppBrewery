f51a5098ecb6681181e43073dcdd47d37afbf2d7
fransHalason <franshalasonn@gmail.com>
2021-09-29 00:03:03 +0700

    day 24 - files, directories, and paths

---
+++ Machine Learning/100 Days of Code - The Complete Python Pro Bootcamp for 2021/Day 24 - Files, Directories and Paths/Day 24 Start/main.py
@@ -0,0 +1,17 @@
+with open("new_file.txt") as file:
+    contents = file.read()
+    print(contents)
+
+# with open("my_file.txt", mode="w") as file:
+#     file.write("New text.")
+
+# with open("my_file.txt", mode="a") as file:
+#     file.write("\nNew text again.")
+
+# with open("Users/LondonAppBrewery/Desktop/new_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# with open("../../Desktop/new_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
